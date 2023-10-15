975ab50ea73780cd6b5e919b5b6acd7815058838
Tom Metcalf <intermet@gmail.com>
2023-10-15 13:02:21 +0100

    add initial setup files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tom Seager Dev
+
+This is the dev portfolio site for Tom Seager (Metcalf) to showcase his web development work.
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='web-address'>dev.tom-seager.com</h1>
+      <h2>
+        <span className="first-line">The Home of </span>
+        <span className="second-line">Tom Seager Dev</span>
+      </h2>
+      <p>Site currently under construction.</p>
+      <p>Please check back for future updates.</p>
+      <section className="siteLinks">
+        <a
+          href="https://www.tom-seager.com"
+          className="siteLinkStyle"
+          target="_blank"
+        >
+          Return to the Tom Seager Main Site
+        </a>
+      </section>
+    </>
+  );
+}
+
+export default App;
